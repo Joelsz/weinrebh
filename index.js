d8ae94bcaa3a4742910a9af2adbc37761ef11a2e
joelsz <joelsz@bhphoto.com>
2017-05-02 12:04:16 -0400

    adding index.js to pass process.env.PORT if available

---
+++ index.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var spawn = require('child_process').spawn;
+
+var commandStart = (process.platform === "win32" ? 'weinre.cmd' : 'weinre');
+
+var child = spawn(commandStart, ['--boundHost', '-all-', '--httpPort', process.env.PORT || '8080']);
+
+child.stdout.on('data', function (data) {
+  console.log('stdout:', data.toString());
+});
+
+child.stderr.on('data', function (data) {
+  console.log('stderr:', data.toString());
+});
+
+child.on('exit', function (code) {
+  console.log('child process exited with code ' + code.toString());
+});
+
+child.on('error', function(error){
+	console.error(error);
+});
